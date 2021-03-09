6521a419535586a57742bb2c2eda6812ade6bc55
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-03-09 14:22:56 +0200

    Mailtrap email test ok

--- server/config.js
+++ server/config.js
@@ -1,7 +1,11 @@
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
   USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD
+  PASSWORD: process.env.PASSWORD,
+  TEST_HOST: process.env.TEST_HOST,
+  TEST_PORT: process.env.TEST_PORT,
+  TEST_USER: process.env.TEST_USER,
+  TEST_PASSWORD: process.env.TEST_PASSWORD
 }
